435555fac7af178d0ff6868fc6cb29a3556c1d30
alector <panagiotispp@gmail.com>
2021-08-03 21:20:30 +0200

    afp

--- contracts/AFP.sol
+++ contracts/AFP.sol
@@ -2,3 +2,42 @@
 pragma solidity ^0.8.5;
 
 import "hardhat/console.sol";
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+contract AFP is ERC721Enumerable, ERC721URIStorage {
+    constructor() ERC721("Alector FoodPrint", "AFP") {}
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        virtual
+        override(ERC721Enumerable, ERC721)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal virtual override(ERC721Enumerable, ERC721) {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    function _burn(uint256 tokenId) internal virtual override(ERC721URIStorage, ERC721) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId) public view virtual override(ERC721URIStorage, ERC721) returns (string memory) {
+        return super.tokenURI(tokenId);
+    }
+
+    // function _baseURI() internal view virtual override(ERC721) returns (string memory) {
+    //     return "https://www.publication.com/nft/";
+    // }
+}
